1b8f6402859dfc4033ae2d54040777380ebcb64e
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2024-12-10 15:05:08 +0400

    fix paths

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,18 +1,12 @@
 import { join } from 'path';
 
-export const appVersion = '2.6.1';
+export const appVersion = '2.6.2';
 
 export const appPath =
   process.env.MODE === 'local'
     ? join(__dirname, '..', '..', 'miner-app', 'dist', 'browser')
     : join(__dirname, '..', 'app');
 
-export const viewPath =
-  process.env.MODE === 'local'
-    ? join(__dirname, 'views')
-    : join(__dirname, '..', '..', 'views');
+export const viewPath = join(__dirname, 'views');
 
-export const assetsPath =
-  process.env.MODE === 'local'
-    ? join(__dirname, 'assets')
-    : join(__dirname, '..', '..', 'assets');
+export const assetsPath = join(__dirname, 'assets');
